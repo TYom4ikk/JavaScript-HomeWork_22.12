5eb1779a0c16e233a4125045bc7a0b129fb3a4ee
TYom4ik <Zhakubekov20032006@gmail.com>
2024-07-08 02:47:50 +0500

    Creating Player and Enemy classes

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,53 @@
 class Creature{
-    constructor(name, hp, damage){
-        
-    }
-    number;
-    id;
+    static number = 0;
+    #id;
     name;
     healthPoints;
     damage;
-    defeat();
+
+    constructor(name, hp, damage){
+        this.number = ++Creature.number;
+        this.#id = Creature.number;
+        this.name = name;
+        this.healthPoints = hp;
+        this.damage = damage;
+    }
+
+    getId() {
+        return this.id;
+    }
+   
+    defeat(){
+        alert('Существо уничтожено!');
+    };
 
 }
 
-Player:Creature{
-    
+class Player extends Creature{
+    #lvl = 0;
+
+    getLvl(){
+        return this.#lvl;
+    }
+
+    attack(other){
+        other.healthPoints -= this.damage;
+        if(other.healthPoints <= 0){
+            other.defeat();
+            this.#lvl++;
+            return true;
+        }
+        return false;
+    }
+}
+
+class Enemy extends Creature{
+    attack(other){
+        other.healthPoints -= this.damage;
+        if(other.healthPoints <= 0){
+            other.defeat();
+            return true;
+        }
+        return false;
+    }
 }
